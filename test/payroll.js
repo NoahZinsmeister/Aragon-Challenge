ad1d548a0316286595fbcbff9ba1d845d54a473c
Noah Zinsmeister <noahwz@gmail.com>
2017-12-21 13:29:00 -0500

    prepping more extensible testing environment

--- test/payroll.js
+++ test/payroll.js
@@ -3,9 +3,43 @@ var EURToken = artifacts.require("./EURToken.sol");
 var Payroll = artifacts.require("./Payroll.sol");
 
 contract('Payroll', function(accounts) {
+    var payrollInstance;
+
     it("should initialize correctly", async function () {
-        let payrollInstance = await Payroll.deployed();
-        let employeeCount = await payrollInstance.getEmployeeCount.call();
-        assert.equal(employeeCount, 0, "> 0 users at the start");
+        payrollInstance = await Payroll.deployed();
+        payrollInstance.getEmployeeCount.call().then(function(count) {
+            assert.equal(count, 0, "> 0 users upon initialization");
+        });
+    });
+
+    // we need access to a persistent EUR token contract for the following tests
+    contract('EURToken', function(accounts) {
+        var EURTokenInstance;
+        var transferAmount = 12;
+
+        it("should accept token funds", async function () {
+            EURTokenInstance = await EURToken.new([100]);
+
+            // transfer 12 EUR to the payroll contract
+            return EURTokenInstance.transfer(payrollInstance.address, transferAmount)
+              .then(function() {
+                return EURTokenInstance.balanceOf(payrollInstance.address);
+            }).then(function(payrollBalance) {
+                assert.equal(
+                    payrollBalance.toNumber(),
+                    transferAmount,
+                    "token transfer failed");
+            });
+        });
+
+        it("should retain token funds", async function () {
+            // check that tokens sent in last test are retained
+            return EURTokenInstance.balanceOf(payrollInstance.address).then(function(balance) {
+                assert.equal(
+                    balance.toNumber(),
+                    transferAmount,
+                    "tokens were lost");
+            });
+        });
     });
 });
